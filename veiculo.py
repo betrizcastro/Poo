bd03659109015490e4d5f180cc4aa62c68dfabd2
Beatriz Castro <mbdecastro2007@gmail.com>
2025-09-04 18:27:16 -0300

    Add files via upload

---
+++ veiculo.py
@@ -0,0 +1,20 @@
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def ligar(self):
+        print(f"O {self.marca} {self.modelo} está ligado.")
+
+    def desligar(self):
+        print(f"O {self.marca} {self.modelo} está desligado.")
+
+    def andar(self):
+        print(f"O {self.marca} {self.modelo} está andando.")
+
+
+carro = Veiculo("Fiat", "Uno")
+carro.ligar()
+carro.desligar()
+carro.andar()
+
